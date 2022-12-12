d03827b82720b1462804c9dd7c2ba39648d1b1b1
Nickk <captaincarmnlgames@gmail.com>
2022-12-12 01:52:06 +0100

    first working version of string parser

---
+++ CoreTests/ParserTest.cs
@@ -0,0 +1,17 @@
+using Core;
+using Xunit;
+
+namespace CoreTests
+{
+    public class ParserTest
+    {
+        [Fact]
+        public void testStringParser()
+        {
+            Parser parser = new Parser();
+            var result = parser.parseRaw("GUID 68e20133a5a944adaeb4c369876a9c1a\n\nHook_Tactical\n\nBlueprint_1_Build 27");
+            Assert.NotNull(result);
+            Assert.Equal(3, result.Count);
+        }
+    }
+}
